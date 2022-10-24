f1dce543bdb5a0e47f23aa1903924b1fe4251347
Gavin Morris <gavin.morris@health.telstra.com>
2022-10-24 17:38:30 +0800

    initial commit

---
+++ OauthHandler/OauthHandler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OauthHandler
+{
+    public class OauthHandler : DelegatingHandler
+    {
+        public OauthHandler(
+            OauthHandlerSettings settings,
+            HttpMessageHandler inner)
+            : base(inner)
+        {
+            Settings = settings;
+        }
+
+        public OauthHandler(
+            OauthHandlerSettings settings)
+            : this(settings, new HttpClientHandler()) { }
+
+        protected async Task ValidateToken()
+        {
+            if (Token != null && Token.ExpiresAt >= (DateTimeOffset.Now - Settings.TokenExpiryThreshold))
+                return;
+
+            try
+            {
+                using (var httpClient = new HttpClient { Timeout = Settings.AuthenticationTimeout })
+                {
+                    var parameters = new Dictionary<string, string>
+                    {
+                        { "grant_type", "client_credentials" },
+                        { "client_id", Settings.ClientId },
+                        { "client_secret", Settings.ClientSecret },
+                    };
+
+                    if (!string.IsNullOrWhiteSpace(Settings.Scopes))
+                        parameters.Add("scope", Settings.Scopes);
+
+                    //Log.Info($"Requesting access token ...");
+                    var response = await httpClient.PostAsync(
+                        Settings.TokenEndpoint, 
+                        new FormUrlEncodedContent(parameters));
+                    //Log.Error($"Response Status Code: {response.StatusCode}{(response.IsSuccessStatusCode ? " (Success)" : string.Empty)}");
+
+                    var result = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Token = JsonSerializer.Deserialize<TokenResponse>(result);
+                        return;
+                    }
+
+                    //Log.Error(new { responseContent = result });
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log.Debug("Exception", ex);
+            }
+
+            //Log.Debug("Token not acquired.");
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            await ValidateToken();
+            request.Headers.Add("Authorization", $"Bearer {Token.AccessToken}");
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        public OauthHandlerSettings Settings { get; internal set; }
+        
+        private TokenResponse Token { get; set; }
+    }
+
+    public class OauthHandlerSettings
+    {
+        public string TokenEndpoint { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string Scopes { get; set; }
+
+        public TimeSpan AuthenticationTimeout { get; set; } = TimeSpan.FromSeconds(10);
+        
+        public TimeSpan TokenExpiryThreshold { get; set; } = TimeSpan.FromSeconds(20);
+    }
+
+    internal class TokenResponse
+    {
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("issues_token_type")]
+        public string IssuesTokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int? ExpiresIn { get; set; }
+
+        [JsonPropertyName("error")]
+        public string Error { get; set; }
+
+        [JsonPropertyName("error_description")]
+        public string ErrorDescription { get; set; }
+
+        public DateTimeOffset CreatedAt { get; internal set; } = DateTimeOffset.Now;
+
+        public DateTimeOffset ExpiresAt => CreatedAt.AddSeconds(ExpiresIn ?? 0);
+    }
+}
---
+++ OauthHandler/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+
+namespace OauthHandler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = LoadSettings();
+
+            var client = new HttpClient(
+                new OauthHandler(settings.Authentication));
+
+            var content = client.GetStringAsync(
+                $"{settings.EndpointUrl}/terminology/fhir/CodeSystem?_summary=true")
+                .GetAwaiter().GetResult();
+
+            Console.WriteLine(content);
+        }
+
+        static Settings LoadSettings()
+        {
+            return new[] { "settings.development.json", "settings.json" }
+                .Where(File.Exists)
+                .Select(x => File.ReadAllText(x))
+                .Select(x => JsonSerializer.Deserialize<Settings>(x))
+                .FirstOrDefault();
+        }
+    }
+
+    internal class Settings
+    {
+        public string EndpointUrl { get; set; }
+        public OauthHandlerSettings Authentication { get; set; }
+    }
+}
